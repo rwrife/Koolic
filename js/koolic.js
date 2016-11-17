481ec6e0baf6e4d1fb1a75679344ced2b0df9277
Ryan Rife <rwrife@gmail.com>
2016-11-17 16:53:42 -0600

    better koolic framework

--- js/koolic.js
+++ js/koolic.js
@@ -1,100 +1,164 @@
-var _$$ = null;
-
-var $$ = document.koolic = function(selector) {
-    if (selector != null && typeof selector === 'string' && selector.length > 0) {
-        var elements = document.querySelectorAll(selector);
-        if (elements.length == 1) { return _createKoolicElement(elements[0]); } else {
-            var kools = [];
-            for (var i = 0; i < elements.length; i++) {
-                kools.push(_createKoolicElement(elements[i]));
-            }
-            return kools;
-        }
-    } else if (selector != null && _$$.IsPlainObject(selector)) {
-        return koolobj(selector);
-    }
-
-    if (_$$ == null) {
-        _$$ = {
-            test: function() {
-                console.log("test");
-            },
-            IsPlainObject: function(obj) { return obj instanceof Object && !(obj instanceof Function || obj.toString() !== '[object Object]' || obj.constructor.name !== 'Object'); },
-            IsDOMObject: function(obj) { return obj instanceof EventTarget; },
-            IsDOMElement: function(obj) { return obj instanceof Node; },
-            IsListObject: function(obj) { return obj instanceof Array || obj instanceof NodeList; },
-            onPropertyChange: function(object, name, value) {},
-            _boundObjs: [],
-            _checkBound: function() {
-                if (_$$._boundObjs != null && _$$._boundObjs.length > 0) {
-                    for (var i = 0; i < _$$._boundObjs.length; i++) {
-                        var obj = _$$._boundObjs[i];
-                        if (obj.value != obj._oldval) {
-                            obj._oldval = obj.value;
-                            obj._obj[obj._name] = obj.value;
-                            obj._onchange(obj._oldval, obj.value);
-                            _$$.onPropertyChange(obj, obj._name, obj.value);
-                        }
-                    }
-                }
-                setTimeout(_$$._checkBound, 5);
-            },
-            _boundElems: [],
-            _koolObjs: []
-        }
-    }
-
-    setTimeout(_$$._checkBound, 5);
-
-    return _$$;
-};
-
-var koolobj = function(object) {
-    for (var i = 0; i < _$$._koolObjs.length; i++) {
-        if (_$$._koolObjs[i]._obj == object) {
-            return _$$._koolObjs[i];
-        }
-    }
-
-    var ko = {
-        _obj: object
+(function() {
+    window.koolic = function(selector) {
+        return $$.find(selector);
     };
+
+    window.koolic.IsPlainObject = function(obj) { return obj instanceof Object && !(obj instanceof Function || obj.toString() !== '[object Object]' || obj.constructor.name !== 'Object'); };
+    window.koolic.IsDOMObject = function(obj) { return obj instanceof EventTarget; };
+    window.koolic.IsDOMElement = function(obj) { return obj instanceof Node; };
+    window.koolic.IsListObject = function(obj) { return obj instanceof Array || obj instanceof NodeList; };
+
+    window.koolic._koolObjs = [];
+    window.koolic._boundObjs = [];
+
+    window.koolic._checkBound = function() {
+        if (window.koolic._boundObjs != null && window.koolic._boundObjs.length > 0) {
+            for (var i = 0; i < window.koolic._boundObjs.length; i++) {
+                var obj = window.koolic._boundObjs[i];
+                obj.validateObject();
+            }
+        }
+        setTimeout(koolic._checkBound, 5);
+    }
+
+    setTimeout(koolic._checkBound, 5);
+
+    window.$$ = window.koolic;
+})();
+
+
+(function(koolic) {
+    koolic.find = function(selector) {
+        if (selector != null && typeof selector === 'string' && selector.length > 0) {
+            var elements = document.querySelectorAll(selector);
+            if (elements.length == 1) { return new KoolicElement(elements[0]); } else {
+                var kools = [];
+                for (var i = 0; i < elements.length; i++) {
+                    kools.push(new KoolicElement(elements[i]));
+                }
+                return kools;
+            }
+        } else if (selector != null && $$.IsPlainObject(selector)) {
+            return new KoolicObject(selector);
+        }
+    };
+})(window.koolic);
+
+
+function KoolicElement(element) {
+    var _ele = element;
+
+    this._boundObjs = [];
+
+    this.hide = function() {
+        element.style.display = 'none';
+        return true;
+    };
+
+    this.show = function() {
+        element.style.display = 'block';
+        return true;
+    };
+
+    this.getElement = function() {
+        return _ele;
+    }
+
+    this.bind = function(koolicBindable) {
+        if (element instanceof HTMLInputElement && element.type == 'text') {
+            element.value = koolicBindable.value;
+            var that = this;
+            element.addEventListener("input", function(event) {
+                for (var i = 0; i < that._boundObjs.length; i++) {
+                    var bindable = that._boundObjs[i];
+                    bindable.setValue(element.value);
+                }
+            });
+        } else if (element instanceof HTMLSelectElement) {
+            element.value = koolicBindable.value;
+            var that = this;
+            element.addEventListener("input", function(event) {
+                for (var i = 0; i < that._boundObjs.length; i++) {
+                    var bindable = that._boundObjs[i];
+                    bindable.setValue(element.value);
+                }
+            });
+        } else if (element instanceof HTMLElement) {
+            element.innerHTML = koolicBindable.value;
+        }
+
+        this._boundObjs.push(koolicBindable);
+        koolicBindable.bind(this);
+        return true;
+    };
+}
+
+
+function KoolicObject(object) {
+    for (var i = 0; i < koolic._koolObjs.length; i++) {
+        if (koolic._koolObjs[i]._obj == object) {
+            return koolic._koolObjs[i];
+        }
+    }
+
+    this._obj = object;
+
+    this.GetObject = function() { return _obj; };
 
     for (var property in object) {
         if (object.hasOwnProperty(property)) {
-            if (typeof object[property] === "string") {
-                ko[property] = koolbindable(object, property);
+            if (typeof object[property] === "string" || typeof object[property] === "number") {
+                this[property] = new KoolicBindable(object, property);
             } else if (typeof object[property] === "object") {
-                ko[property] = koolobj(object[property]);
+                this[property] = new KoolicObject(object[property]);
             } else {
-                ko[property] = object[property];
+                this[property] = object[property];
             }
         }
     }
 
-    _$$._koolObjs.push(ko);
-
-    return ko;
+    koolic._koolObjs.push(this);
 };
 
-koolbindable = function(object, property) {
-    var kb = {
-        _name: property,
-        _obj: object,
-        _oldval: object[property],
-        value: object[property],
-        _onchange: function(oldval, newval) {
+function KoolicBindable(object, property) {
+    var _name = property,
+        _obj = object,
+        _oldval = object[property],
+        _notify = [];
 
-            if (this._notify && this._notify.length > 0) {
-                for (var i = 0; i < this._notify.length; i++) {
-                    var element = this._notify[i].element;
-                    if (element instanceof HTMLInputElement && element.type == 'text') {
-                        element.value = this.value;
-                    } else if (element instanceof HTMLSelectElement) {
-                        //for(var s=0;s<element.options.length)
-                        element.value = this.value;
-                        console.log(this.value + ' ' + element.value);
+    var bindable = this;
 
+    this.value = object[property];
+
+    this.bind = function(koolicElement) {
+        _notify.push(koolicElement);
+    }
+
+    this.hasChange = function() {
+        if (_oldval != _obj[_name] || _obj[_name] != this.value) return true;
+        else return false;
+    }
+
+    this.validateObject = function() {
+        if (this.hasChange()) {
+            if (this.value != _oldval) {
+                this.setValue(this.value);
+                _oldval = this.value;
+            } else if (_obj[_name] != this.value) {
+                this.setValue(_obj[_name]);
+            }
+        }
+    }
+
+    this.setValue = function(value) {
+        if (value != this.value) {
+            _obj[_name] = value;
+            this.value = value;
+            if (_notify && _notify.length > 0) {
+                for (var i = 0; i < _notify.length; i++) {
+                    var element = _notify[i].getElement();
+                    if ((element instanceof HTMLInputElement && element.type == 'text') || element instanceof HTMLSelectElement) {
+                        element.value = this.value;
                     } else if (element instanceof HTMLElement) {
                         element.innerHTML = this.value;
                     }
@@ -102,76 +166,10 @@ koolbindable = function(object, property) {
             }
 
             if (this && this.onchange) {
-                this.onchange(oldval, newval);
-            }
-        },
-        _notify: []
-    };
-
-    _$$._boundObjs.push(kb);
-
-    return kb;
-};
-
-
-var _ke = {};
-
-function _createKoolicElement(element) {
-    var ke = {
-        element: element,
-        hide: function() {
-            element.style.display = 'none';
-            return true;
-        },
-        show: function() {
-            element.style.display = 'block';
-            return true;
-        },
-        bind: function(bindable) {
-            if (element instanceof HTMLInputElement && element.type == 'text') {
-                element.value = bindable.value;
-                var that = this;
-                element.addEventListener("input", function(event) {
-                    for (var i = 0; i < that._boundObjs.length; i++) {
-                        var bindable = that._boundObjs[i];
-                        bindable.value = element.value;
-                    }
-                });
-            } else if (element instanceof HTMLSelectElement) {
-                element.value = bindable.value;
-                var that = this;
-                element.addEventListener("input", function(event) {
-                    for (var i = 0; i < that._boundObjs.length; i++) {
-                        var bindable = that._boundObjs[i];
-                        bindable.value = element.value;
-                    }
-                });
-            } else if (element instanceof HTMLElement) {
-                element.innerHTML = bindable.value;
-            }
-
-            this._boundObjs.push(bindable);
-            bindable._notify.push(this);
-            return true;
-        },
-        _boundObjs: [],
-        text: function(value) {
-            if (typeof value === 'undefined') {
-                return this.element.innerHTML;
-            } else {
-                this.element.innerHTML = value;
-                return true;
+                this.onchange(_oldval, value);
             }
         }
+    }
 
-    };
-
-
-
-    _$$._boundElems.push(ke);
-
-    return ke;
+    koolic._boundObjs.push(this);
 };
-
-
-document.koolic();
